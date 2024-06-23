d7ce818feaf5b2b55b58a30f636eca6d3b672a0c
prashantsingh181 <prashant.singh181@gmail.com>
2024-06-23 23:19:45 +0530

    added similar items in product details page and fixed the bug of scroll to top component

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,13 +1,20 @@
 import { useParams } from "react-router-dom"
 import { useSelector } from "react-redux";
-import { productByIdSelector } from "../redux/slices/products";
+import { productByIdSelector, productsByCategorySelector } from "../redux/slices/products";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { FaRegHeart } from "react-icons/fa";
 import Rating from "../components/Rating";
+import ProductListRow from "../components/ProductListRow";
 
 const ProductDetails = () => {
+    // getting productId from URL and getting corresponding product details from redux store
     const { productId } = useParams();
     const product = useSelector((state) => productByIdSelector(state, Number(productId)))
+
+    // extracting category of product and getting similar items in that category
+    const category = product && product.category;
+    const similarCategoryProducts = useSelector((state) => productsByCategorySelector(state, category))
+
     return (<>
         {product && <div className="grid grid-flow-row grid-cols-1 lg:grid-cols-2">
             <section className="h-96 flex justify-center items-center">
@@ -20,6 +27,8 @@ const ProductDetails = () => {
                 </div>
                 <hr />
                 <div className="flex flex-col"><span className="font-bold text-red-500 text-xl">{import.meta.env.VITE_CURRENCY}{product.price}</span><span>inclusive of all taxes</span></div>
+                {/* Quantity for product here */}
+                {/* cart and wishlist button */}
                 <div className="flex gap-10">
                     <button className="bg-accent text-white rounded px-6 py-3 flex gap-2 items-center text-lg"><AiOutlineShoppingCart className="text-xl" />
                         <span>ADD TO CART</span></button>
@@ -29,12 +38,19 @@ const ProductDetails = () => {
                     </button>
                 </div>
                 <hr />
+                {/* product description */}
                 <div>
                     <h3 className="text-xl font-bold">Description</h3>
                     <p className="py-2">{product.description}</p>
                 </div>
             </section>
-        </div>}p
+        </div>}
+        <hr className="my-8" />
+        {/* similar items */}
+        {category && <section className="flex flex-col gap-4 my-4">
+            <h3 className="text-xl font-bold">See more in {category}</h3>
+            <ProductListRow products={similarCategoryProducts} />
+        </section>}
     </>
     )
 }
